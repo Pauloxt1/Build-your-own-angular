5a80d41df4dec9233b74094bddde22ce443f56f8
Paulo Roberto <psface1@gmail.com>
2016-06-29 19:56:38 -0300

    Starting building base of scopes witch  and

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Build Your Own AngularJS
+[![Build Status](https://travis-ci.org/Pauloxt1/Build-your-own-angular.svg?branch=master)](https://travis-ci.org/Pauloxt1/Build-your-own-angular)
 This is not the official repository of the book [Build Your Own AngularJS](http://teropa.info/build-your-own-angular). This repository contains my implementation of the code.
---
+++ src/scope.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function Scope(){
+  this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function(watchFn, listenerFn){
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  }
+  this.$$watchers.push(watcher);
+}
+
+Scope.prototype.$digest = function(){
+    var self = this;
+    var newValue, oldValue;
+
+  _.forEach(this.$$watchers, function(watcher){
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+
+    if(newValue != oldValue){
+        watcher.listenerFn(newValue, oldValue, self);
+        watcher.last = newValue;
+    }
+
+  });
+}

5fd16cbd46ba3ae85ff6fb8d16d899be76a8e4de
Paulo Roberto <psface1@gmail.com>
2016-06-30 19:49:55 -0300

    Finishing the basic of digest and watch

--- src/scope.js
+++ src/scope.js
@@ -6,14 +6,17 @@ function initWatchVal(){
 
 function Scope(){
   this.$$watchers = [];
+  this.$$lastDirtyWatch = null;
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn){
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq){
   var watcher = {
     watchFn: watchFn,
     listenerFn: listenerFn || function(){},
+    valueEq: !!valueEq,
     last: initWatchVal
   };
+  this.$$lastDirtyWatch = null;
   this.$$watchers.push(watcher);
 };
 
@@ -25,10 +28,13 @@ Scope.prototype.$$digestOnce = function(){
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
 
-    if(newValue != oldValue){
+    if(!self.$$areEqual(newValue, oldValue, watcher.valueEq)){
+        self.$$lastDirtyWatch = watcher;
         watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
-        watcher.last = newValue;
+        watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
         dirty = true;
+    } else if(self.$$lastDirtyWatch == watcher){
+        return false;
     }
   });
   return dirty;
@@ -37,6 +43,7 @@ Scope.prototype.$$digestOnce = function(){
 Scope.prototype.$digest = function(){
   var ttl = 10;
   var dirty;
+  this.$$lastDirtyWatch = null;
   do{
     if(dirty && !(ttl--)){
       throw "10 digest interations reached";
@@ -45,3 +52,11 @@ Scope.prototype.$digest = function(){
 
   }while(dirty);
 };
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq){
+  if(valueEq){
+    return _.isEqual(newValue, oldValue);
+  } else {
+    return newValue === oldValue || (typeof newValue == 'number' && typeof oldValue == 'number' && isNaN(newValue) && isNaN(oldValue));
+  }
+}
